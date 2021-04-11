b8d2d6c899b48119fd493e1ec160a25b5b337b69
Alev Orfi <aorfi1234@gmail.com>
2021-04-11 12:00:19 -0400

    Figures from Sequential Run

--- PlotOrganizedCompare.py
+++ PlotOrganizedCompare.py
@@ -1,19 +1,8 @@
 import json
-from qutip import *
 import numpy as np
-import scipy
-import cmath
-import matplotlib.pyplot as plt
-import itertools
-from numpy.random import seed
-from numpy.random import rand
-from itertools import product
-from scipy.misc import derivative
-from scipy import optimize
-from functools import wraps
-from time import time
-from scipy.optimize import minimize
 from matplotlib import gridspec
+import matplotlib
+import matplotlib.pyplot as plt
 plt.style.use('seaborn')
 
 
@@ -53,17 +42,19 @@ engErrVarAll = []
 stateErrVarAll = []
 runTimeVarAll = []
 
-#
-NListVar = np.arange(2,12)
+NListVar = np.arange(2,14)
 for i in range(len(NListVar)):
     N = NListVar[i]
     M=N
-    dataLocation = "Data/21-03-23/clusterVarN"+str(N)+"M" + str(M)+".json"
+    dataLocation = "Data/21-04-13/varN"+str(N)+"M" + str(M)+".json"
     saved = []
     with open(dataLocation) as file:
         for line in file:
             saved.append(json.loads(line))
     engErrSR, stateErrSR, runTimeSR= saved
+    engErrSR.pop(0)
+    stateErrSR.pop(0)
+    runTimeSR.pop(0)
     engErrVarAll.append(engErrSR)
     stateErrVarAll.append(stateErrSR)
     runTimeVarAll.append(runTimeSR)
@@ -77,24 +68,23 @@ NListSR = np.arange(2, 14)
 for i in range(len(NListSR)):
     N = NListSR[i]
     M = N
-    dataLocation = "Data/21-04-06/N" + str(N) + "M" + str(M) + ".json"
+    dataLocation = "Data/21-04-13/N" + str(N) + "M" + str(M) + ".json"
     saved = []
     with open(dataLocation) as file:
         for line in file:
             saved.append(json.loads(line))
     engErrSR, stateErrSR, runTimeSR = saved
+    engErrSR.pop(0)
+    stateErrSR.pop(0)
+    runTimeSR.pop(0)
     engErrSRAll.append(engErrSR)
     stateErrSRAll.append(stateErrSR)
     runTimeSRAll.append(runTimeSR)
 
 
+
 # **** Get averages *****
 cutOff = 0.01 #### *******
-
-avEngErrHeiF = []
-avStateErrHeiF = []
-avRunTimeHeiF = []
-runsOverHeiF = []
 
 avEngErrVar = []
 avStateErrVar = []
@@ -106,22 +96,6 @@ avStateErrSR = []
 avRunTimeSR = []
 runsOverSR = []
 
-# heiRem = 0
-# for i in range(len(engErrHeiFAll)):
-#     #remove nan from averages
-#     engErrHeiFAllClean = [x for x in engErrHeiFAll[i] if str(x) != 'nan']
-#     heiRem += len(engErrHeiFAll[i])- len(engErrHeiFAllClean)
-#     avEngErrTemp = np.sum(engErrHeiFAllClean) / (50)
-#     avEngErrHeiF.append(avEngErrTemp)
-#     stateErrHeiFAllClean = [x for x in stateErrHeiFAll[i] if str(x) != 'nan']
-#     avStateErrTemp = np.sum(stateErrHeiFAllClean) / (50)
-#     avStateErrHeiF.append(avStateErrTemp)
-#     avRunTimeTemp = np.sum(runTimeHeiFAll[i]) / (50)
-#     avRunTimeHeiF.append(avRunTimeTemp)
-#     runsOverTemp = sum(j > cutOff for j in engErrHeiFAll[i])
-#     runsOverHeiF.append(runsOverTemp)
-# print('Number Removed Hei', heiRem)
-#
 varRem = 0
 for i in range(len(engErrVarAll)):
     # remove nan from averages
@@ -153,80 +127,82 @@ for i in range(len(engErrSRAll)):
     runsOverSRTemp = sum(j > cutOff for j in engErrSRAll[i])
     runsOverSR.append(runsOverSRTemp)
 print('Number Removed cs', csRem)
+
+
+
 #
-# #***** Histogram ****
-# index = 9
-# N = index+2
-# M = N
-# hisIt= np.arange(10)
-# #plt.figure(constrained_layout=True)
-# plt.figure(figsize=(10,10))
-# engErr = [engErrSRAll[index],engErrVarAll[index]]#,engErrHeiFAll[index]]
-# stateErr= [stateErrSRAll[index],stateErrVarAll[index]]#,stateErrHeiFAll[index]]
-# runTime= [runTimeSRAll[index],runTimeVarAll[index]]#,runTimeHeiFAll[index]]
+# for i in range(len(engErrSRAll)):
+#     #***** Histogram ****
+#     index = i
+#     N = index+2
+#     M = N
+#     hisIt= np.arange(49)
+#     #plt.figure(constrained_layout=True)
+#     plt.figure(figsize=(10,10))
+#     engErr = [engErrSRAll[index],engErrVarAll[index]]
+#     stateErr= [stateErrSRAll[index],stateErrVarAll[index]]
+#     runTime= [runTimeSRAll[index],runTimeVarAll[index]]
+#
+#     labels = ['Central Spin with Constant A','Central Spin with Varying A']
+#     colors = ['blue', 'green']#, 'red']
+#     ttl = plt.suptitle("Stochastic Reconfiguration with"+ r" $\alpha = 1$"+" \n N = " + str(N)+", M = " + str(M),size =20)
+#     gs = gridspec.GridSpec(ncols=3, nrows=4, hspace = 0.4)
+#     ttl.set_position([.5, 0.94])
+#     ax1 = plt.subplot(gs[0, 0])
+#     ax1.hist(engErr, bins=10, color = colors, label=labels)
+#     ax1.set_xlabel(r"${\Delta} E = |E_{RBM}-E_{ED}|$",size = 15)
+#     ax2 = plt.subplot(gs[0, 1])
+#     ax2.hist(stateErr, bins=10, color = colors, label=labels)
+#     ax2.set_xlabel(r"$1-|<\Psi_{RBM}|\Psi_{ED}>|^2$",size = 15)
+#
+#     ax3 = plt.subplot(gs[0, 2])
+#     ax3.hist(runTime, bins=10, color = colors, label=labels)
+#     ax3.set_xlabel("Runtime (s)",size = 15)
+#
+#     ax4 = plt.subplot(gs[1, :])
+#     ax4.scatter(hisIt, engErrSRAll[index], color = colors[0], label=labels[0], marker = '^')
+#     ax4.scatter(hisIt, engErrVarAll[index], color = colors[1], label=labels[1], marker = '>')
+#     ax4 .set_ylabel(r"${\Delta} E = |E_{RBM}-E_{ED}|$", size = 15)
+#
+#     ax4 = plt.subplot(gs[2, :])
+#     ax4.scatter(hisIt, stateErrSRAll[index], color = colors[0], label=labels[0], marker = '^')
+#     ax4.scatter(hisIt, stateErrVarAll[index], color = colors[1], label=labels[1], marker = '>')
+#     ax4 .set_ylabel(r"$1-|<\Psi_{RBM}|\Psi_{ED}>|^2$", size = 15)
+#
+#     ax5 = plt.subplot(gs[3, :])
+#     ax5.scatter(hisIt, runTimeSRAll[index], color = colors[0], label=labels[0], marker = '^')
+#     ax5.scatter(hisIt, runTimeVarAll[index], color = colors[1], label=labels[1], marker = '>')
+#     ax5.set_xlabel("Run Number",size = 15)
+#     ax5 .set_ylabel("Runtime (s)", size = 15)
+#     ax5.legend(labels, loc = (0.1, -0.6),fontsize = 12,ncol=3)
+#     plt.savefig("Figures/21-04-13/N"+str(N)+"his.png")
+#     plt.show()
 
-# labels = ['Central Spin with Constant A','Central Spin with Varying A']#,'Heisenberg with Field']
-# colors = ['blue', 'green']#, 'red']
-# ttl = plt.suptitle("Stochastic Reconfiguration with"+ r" $\alpha = 1$"+" \n N = " + str(N)+", M = " + str(M),size =20)
-# gs = gridspec.GridSpec(ncols=3, nrows=4, hspace = 0.4)
-# ttl.set_position([.5, 0.94])
-# ax1 = plt.subplot(gs[0, 0])
-# ax1.hist(engErr, bins=10, color = colors, label=labels)
-# ax1.set_xlabel("$\Delta E = |E_{RBM}-E_{ED}|$",size = 15)
-# ax2 = plt.subplot(gs[0, 1])
-# ax2.hist(stateErr, bins=10, color = colors, label=labels)
-# ax2.set_xlabel("$1-|<\Psi_{RBM}|\Psi_{ED}>|^2$",size = 15)
-
-# ax3 = plt.subplot(gs[0, 2])
-# ax3.hist(runTime, bins=10, color = colors, label=labels)
-# ax3.set_xlabel("Runtime (s)",size = 15)
-
-# ax4 = plt.subplot(gs[1, :])
-# ax4.scatter(hisIt, engErrSRAll[index], color = colors[0], label=labels[0], marker = '^')
-# #ax4.scatter(hisIt, engErrVarAll[index], color = colors[1], label=labels[1], marker = '>')
-# #ax4.scatter(hisIt, engErrHeiFAll[index], color = colors[2], label=labels[2], marker = '<')
-# ax4 .set_ylabel("$\Delta E = |E_{RBM}-E_{ED}|$", size = 15)
-
-# ax4 = plt.subplot(gs[2, :])
-# ax4.scatter(hisIt, stateErrSRAll[index], color = colors[0], label=labels[0], marker = '^')
-# #ax4.scatter(hisIt, stateErrVarAll[index], color = colors[1], label=labels[1], marker = '>')
-# #ax4.scatter(hisIt, stateErrHeiFAll[index], color = colors[2], label=labels[2], marker = '<')
-# ax4 .set_ylabel("$1-|<\Psi_{RBM}|\Psi_{ED}>|^2$", size = 15)
-
-# ax5 = plt.subplot(gs[3, :])
-# ax5.scatter(hisIt, runTimeSRAll[index], color = colors[0], label=labels[0], marker = '^')
-# #ax5.scatter(hisIt, runTimeVarAll[index], color = colors[1], label=labels[1], marker = '>')
-# #ax5.scatter(hisIt, runTimeHeiFAll[index], color = colors[2], label=labels[2], marker = '<')
-# ax5.set_xlabel("Run Number",size = 15)
-# ax5 .set_ylabel("Runtime (s)", size = 15)
-# ax5.legend(labels, loc = (0.1, -0.6),fontsize = 12,ncol=3)
-# plt.show()
-# # #
+# #
 #
 # #***** Run Time Scaling ******
-
+#
 plt.figure(constrained_layout=True)
 plt.figure(figsize=(8,8))
 ttl = plt.suptitle("Runtime Scaling \n"+r" $\alpha = 1$" ,size =18)
 gs = gridspec.GridSpec(ncols=1, nrows=1, hspace = 0.4)
 ttl.set_position([.5, 0.98])
-#labels = ['RBM Constant A','RBM Varying A','ED RBM Constant A','ED Varying A']
-labels = ['RBM ','RBM Varying A','Exact Diagonalization','ED Varying A']
+labels = ['RBM Constant A','RBM Varying A','ED Constant A','ED Varying A']
 colors = ['blue', 'green', 'red']
 ax1 = plt.subplot(gs[0, 0])
 ax1.scatter(NListSR, avRunTimeSR, color = colors[0], label=labels[0], marker = '^')
-#ax1.scatter(NListVar, avRunTimeVar, color = colors[1], label=labels[1], marker = '>')
+ax1.scatter(NListVar, avRunTimeVar, color = colors[1], label=labels[1], marker = '>')
 ax1.set_xlabel("N",size = 15)
 ax1.set_ylabel("Average Runtime (s)",size = 15)
 #ax2 = ax1.twinx()
 ax1.scatter(NListExact, RunTimeExact, color = colors[2], label=labels[2], marker = '>')
 #ax1.scatter(NListExactVar, RunTimeExactVar, color = 'black', label=labels[3], marker = '>')
-ax1.legend(loc = (0.2, -0.13),fontsize = 12,ncol=4)
+ax1.legend(loc = (0.0, -0.13),fontsize = 12,ncol=4)
 ax1.set_yscale('log')
 #ax1.set_xscale('log')
 plt.show()
 
-# # ***** Energy Error Scaling ******
+# ***** Energy Error Scaling ******
 # plt.figure(constrained_layout=True)
 # plt.figure(figsize=(8,8))
 # labels = ['RBM Constant A', 'RBM Varying A','Heisenberg with Field']
@@ -242,7 +218,7 @@ plt.show()
 # ax1.set_xlabel("N",size = 15)
 # ax1.set_ylabel("Energy Error",size = 15)
 # ax1.legend(labels, loc = (0, -0.13),fontsize = 12,ncol=3)
-# ax1.set_yscale('log')
+# #ax1.set_yscale('log')
 # plt.show()
 
 
@@ -267,12 +243,12 @@ plt.show()
 # ax1.set_yscale('log')
 # plt.show()
 
-# # # ***** Number of Runs******
+# # # # ***** Number of Runs******
 # plt.figure(constrained_layout=True)
 # plt.figure(figsize=(8,8))
 # labels = ['Central Spin with Constant A', 'Central Spin with Varying A']#,'Heisenberg with Field']
 # colors = ['blue', 'green']#, 'red']
-# ttl = plt.suptitle("Number of Runs with Energy Error above "+str(cutOff)+"\n"+r"Stochastic Reconfiguration with $\alpha = 1$" ,size =20)
+# ttl = plt.suptitle("Number of Runs with Energy Error above "+str(cutOff)+"\n"+r" $\alpha = 1$" ,size =20)
 # gs = gridspec.GridSpec(ncols=1, nrows=1, hspace = 0.4)
 # ttl.set_position([.5, 0.97])
 # ax1 = plt.subplot(gs[0, 0])
@@ -285,8 +261,8 @@ plt.show()
 # ax1.legend(labels, loc = (0, -0.13),fontsize = 12,ncol=3)
 # #ax1.set_yscale('log')
 # plt.show()
-#
-# # # # ***** Average Run Time******
+# #
+# # # ***** Average Run Time******
 # plt.figure(constrained_layout=True)
 # plt.figure(figsize=(8,8))
 # labels = ['Central Spin with Constant A', 'Central Spin with Varying A','Heisenberg with Field']
